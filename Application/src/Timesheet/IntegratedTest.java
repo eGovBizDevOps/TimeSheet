df44b74d4a1ba99704b9bff1130fbf08162602bf
bssintern2 <jricketts@egovja.com>
2015-08-12 11:21:16 -0500

    Console log messages

--- Application/src/Timesheet/IntegratedTest.java
+++ Application/src/Timesheet/IntegratedTest.java
@@ -8,19 +8,22 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class IntegratedTest {
    public static void main(String[] args) throws InterruptedException {
-   
+	  int i = 0;
       // Instantiate the RC Server
+	  System.out.println(i+1);
       WebDriver driver = new FirefoxDriver();
       
-
+      System.out.println(i+1);
       driver.manage().window().maximize();
 
       driver.get("http://localhost:8080/Application");
+      System.out.println(i+1);
       
 
       String name = driver.findElement(By.id("head")).getText();//Locating element by class name and store its text to variable datentime.
       System.out.println(name);
-      
+
+      System.out.println(i+1);
 
       driver.findElement(By.xpath(".//*[@id='j_idt6']/input[2]")).sendKeys("@@@@");
       driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
@@ -32,8 +35,10 @@ public class IntegratedTest {
       driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
       driver.findElement(By.xpath(".//*[@id='j_idt6']/input[6]")).sendKeys("6:00pm");
       driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+      System.out.println(i+1);
       
       driver.findElement(By.xpath(".//*[@id='j_idt6']/input[7]")).click();
+      System.out.println(i+1);
       
       driver.quit();
    }
